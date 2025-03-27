227cbe4eb892e17971d4e948626e32f613f73fa8
Harish <h.kush2005@gmail.com>
2025-03-27 09:45:33 +0530

    Update PCLoader.jsx

--- src/pages/PCLoader.jsx
+++ src/pages/PCLoader.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import { Html, useProgress } from "@react-three/drei";
+import { motion } from "framer-motion";
+import React from "react";
 
 const PCLoader = () => {
-    return (
-        <div className="flex justify-center items-center w-full h-full">
-          <div className="w-12 h-12 border-4 border-blue-400 border-t-transparent rounded-full animate-spin"></div>
-        </div>
-    );
-}
+  const { progress } = useProgress();
 
-export default PCLoader
+  return (
+    <Html as='div' center>
+      <div className="flex flex-col items-center">
+       
+        <motion.div
+          animate={{ rotate: 360 }}
+          transition={{ repeat: Infinity, duration: 1.5, ease: "linear" }}
+          className="w-16 h-16 border-4 border-blue-400 border-t-transparent rounded-full"
+        />
+    
+        <motion.p
+          className="mt-4 text-lg font-bold text-gray-200"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          transition={{ duration: 0.5 }}
+        >
+          {progress.toFixed(0)}%
+        </motion.p>
+      </div>
+    </Html>
+  );
+};
+
+export default PCLoader;
